ce8bb48187301bbd8a53d672480fcd18138cced1
samuelhackwill <hackmew@gmail.com>
2025-01-12 13:45:52 +0100

    Add clic logging server side

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import { cleanData } from 'jquery'
 import { streamer } from '../both/streamer.js'
 
 // Import the WebSocket library
@@ -11,7 +10,7 @@ const PING_INTERVAL = 5000
 queue = []
 
 console.log(
-  'Hi! this is Samuel from Tryhard. Just so that you know, the WebSocket server for the raspberrys is running on ws://localhost:8080. Please tell the mouse-grabr.py to talk to that adress. see ya',
+  'Hi! this is Samuel from Tryhard. Just so that you know, the WebSocket server for the raspberrys is running on ws://<this machine>:8080. Please tell the mouse-grabr.py to talk to that adress. see ya',
 )
 
 addToQueue = function (data) {
@@ -27,10 +26,21 @@ mergeQueue = function (objectsArray) {
   objectsArray.forEach((obj, i) => {
     const client = obj.client
     if (!merged[client]) {
-      merged[client] = { client: client, x: 0, y: 0 }
+      // first look if that client is already known ie has sent more than one event during this 16ms loop
+      merged[client] = { client: client, x: 0, y: 0, buttonEvents: [] }
     }
+    // ok so if this guy has already told us to move, just make a sum of all past movement instructions
     merged[client].x += obj.x || 0
     merged[client].y += obj.y || 0
+
+    // also check if he's doing anything with the mouse buttons
+    // on utilise un array parce que contrairement aux mouvements qui peuvent être additionnés et écrasés,
+    // on veut jouer tous les mouse events les uns après les autres ; c'est à dire si un mousepressed et un mouserelease
+    // ont été effectués à l'intérieur de 16.6ms, on veut pas que l'un soit ignoré, on veut que le client les joue tous deux.
+    // bon c'est sans doute un gros edge case des familles, mais en même temps on sait qu'un clic peut être pressé et released à l'intérieur de 16ms, easily.
+    if (obj.code?.length > 0) {
+      merged[client].buttonEvents.push({ code: obj.code[0], value: obj.value })
+    }
   })
 
   // console.log(merged)
