37d4c0478d35b13d4e0c65ae46bc49a27c9371de
samuelhackwill <hackmew@gmail.com>
2025-01-20 16:22:17 +0100

    Remove old stuff for tryhard.samuel.ovh

--- server/main.js
+++ server/main.js
@@ -3,16 +3,6 @@ import { streamer } from '../both/streamer.js'
 import './parser.js'
 import './server.js'
 
-const description = "Playtest de Novembre '24 au Melies."
-
-WebApp.connectHandlers.use('/api/hello', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*') // Allow all origins (use specific domains for more security)
-  res.setHeader('Content-Type', 'text/plain')
-
-  res.write(description)
-  res.end()
-})
-
 streamer.allowRead('all')
 streamer.allowWrite('all')
 
